7159bb0287c942191aebaa01693ef39a909c34df
tercsd <tercsd@gmail.com>
2023-01-19 16:21:50 +0100

    add more input to Hotels module

--- el-proyecte-grande-frontend/src/Components/Content/HotelModule/Hotels.jsx
+++ el-proyecte-grande-frontend/src/Components/Content/HotelModule/Hotels.jsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -9,6 +11,7 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import EditIcon from '@mui/icons-material/Edit';
 import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+import TextField from '@mui/material/TextField';
 
 function createData(id, name, address, status, stars, floors, rooms) {
     return { id, name, address, status, stars, floors, rooms };
@@ -26,41 +29,57 @@ const rows = [
 
 const Hotels = () => {
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center">Id</TableCell>
-                        <TableCell align="center">Name</TableCell>
-                        <TableCell align="center">Address</TableCell>
-                        <TableCell align="center">Status</TableCell>
-                        <TableCell align="center">Stars</TableCell>
-                        <TableCell align="center">Floors</TableCell>
-                        <TableCell align="center">Rooms</TableCell>
-                        <TableCell align="center">Edit</TableCell>
-                        <TableCell align="center">Set status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell align="center">{row.id}</TableCell>
-                            <TableCell align="center">{row.name}</TableCell>
-                            <TableCell align="center">{row.address}</TableCell>
-                            <TableCell align="center">{row.status}</TableCell>
-                            <TableCell align="center">{row.stars}</TableCell>
-                            <TableCell align="center">{row.floors}</TableCell>
-                            <TableCell align="center">{row.rooms}</TableCell>
-                            <TableCell align="center"><Button variant="text"><EditIcon/></Button></TableCell>
-                            <TableCell align="center"><Button variant="text"><DisabledByDefaultIcon /></Button></TableCell>
+        <div className="Hotels">
+            <Box sx={{ marginY: 1 }}>
+                <Grid container
+                    direction="row"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Grid item xs={12} md={9} >
+                        <Button variant="text">Add new</Button>
+                    </Grid>
+                    <Grid item xs={12} md={3}>
+                        <TextField id="outlined-basic" label="Filter" variant="outlined" size="small" />
+                    </Grid>
+                </Grid>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Id</TableCell>
+                            <TableCell align="center">Name</TableCell>
+                            <TableCell align="center">Address</TableCell>
+                            <TableCell align="center">Status</TableCell>
+                            <TableCell align="center">Stars</TableCell>
+                            <TableCell align="center">Floors</TableCell>
+                            <TableCell align="center">Rooms</TableCell>
+                            <TableCell align="center">Edit</TableCell>
+                            <TableCell align="center">Set status</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell align="center">{row.id}</TableCell>
+                                <TableCell align="center">{row.name}</TableCell>
+                                <TableCell align="center">{row.address}</TableCell>
+                                <TableCell align="center">{row.status}</TableCell>
+                                <TableCell align="center">{row.stars}</TableCell>
+                                <TableCell align="center">{row.floors}</TableCell>
+                                <TableCell align="center">{row.rooms}</TableCell>
+                                <TableCell align="center"><Button variant="text"><EditIcon /></Button></TableCell>
+                                <TableCell align="center"><Button variant="text"><DisabledByDefaultIcon /></Button></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
