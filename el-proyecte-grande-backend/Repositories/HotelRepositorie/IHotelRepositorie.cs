184124f8c8219ab0df16b3a21e6edd2c1e232523
Plavecz Adam <plaveczadam79@gmail.com>
2023-01-31 15:20:01 +0100

    add IHotelRepositorie

---
+++ el-proyecte-grande-backend/Repositories/HotelRepositorie/IHotelRepositorie.cs
@@ -0,0 +1,13 @@
+﻿using el_proyecte_grande_backend.Models.Entities;
+
+namespace el_proyecte_grande_backend.Repositories.HotelRepositorie
+{
+    public interface IHotelRepositorie
+    {
+        public IEnumerable<Hotel> GetAllHotels();
+        public Hotel GetHotel(int id);
+        public Hotel AddHotel();
+        public Hotel UpdateHotel(int hotelId, Hotel hotel);
+        public Hotel SetHotelStatus(int hotelId, int status);
+    }
+}
