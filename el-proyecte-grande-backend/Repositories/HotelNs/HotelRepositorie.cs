7a71dd28db50fae89814cd8333ed9fdb419d1340
Plavecz Adam <plaveczadam79@gmail.com>
2023-01-31 15:46:25 +0100

    add HotelRepositorie solve namespace issue in IHotelRepositorie

---
+++ el-proyecte-grande-backend/Repositories/HotelNs/HotelRepositorie.cs
@@ -0,0 +1,38 @@
+﻿using el_proyecte_grande_backend.Data;
+
+namespace el_proyecte_grande_backend.Repositories.HotelNs
+{
+    public class HotelRepositorie : IHotelRepositorie
+    {
+        private readonly GrandeHotelContext _context;
+        public HotelRepositorie(GrandeHotelContext context)
+        {
+            _context = context;
+        }
+
+        public Models.Entities.Hotel AddHotel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.Entities.Hotel> GetAllHotels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Entities.Hotel GetHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Entities.Hotel SetHotelStatus(int hotelId, int status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Entities.Hotel UpdateHotel(int hotelId, Models.Entities.Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- el-proyecte-grande-backend/Repositories/HotelNs/IHotelRepositorie.cs
+++ el-proyecte-grande-backend/Repositories/HotelNs/IHotelRepositorie.cs
@@ -1,13 +1,13 @@
 ﻿using el_proyecte_grande_backend.Models.Entities;
 
-namespace el_proyecte_grande_backend.Repositories.HotelRepositorie
+namespace el_proyecte_grande_backend.Repositories.HotelNs
 {
     public interface IHotelRepositorie
     {
         public IEnumerable<Hotel> GetAllHotels();
         public Hotel GetHotel(int id);
         public Hotel AddHotel();
-        public Hotel UpdateHotel(int hotelId, Hotel hotel);
+        public Hotel UpdateHotel(int hotelId, Models.Entities.Hotel hotel);
         public Hotel SetHotelStatus(int hotelId, int status);
     }
 }
