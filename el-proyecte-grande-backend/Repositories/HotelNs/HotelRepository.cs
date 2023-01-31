1905ba55128a271f1a622080d7055c302d6a9ab6
Plavecz Adam <plaveczadam79@gmail.com>
2023-01-31 18:00:11 +0100

    implement HotelRepository

---
+++ el-proyecte-grande-backend/Repositories/HotelNs/HotelRepository.cs
@@ -0,0 +1,69 @@
+﻿using el_proyecte_grande_backend.Data;
+using el_proyecte_grande_backend.Models.Entities;
+using el_proyecte_grande_backend.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace el_proyecte_grande_backend.Repositories.HotelNs
+{
+    public class HotelRepository : IHotelRepository
+    {
+        private readonly GrandeHotelContext _context;
+        public HotelRepository(GrandeHotelContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Hotel> AddHotel(Hotel hotel)
+        {
+            _context.Hotels.Add(hotel);
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+
+        public async Task<IEnumerable<Hotel>> GetAllHotels()
+        {
+            return await _context.Hotels.ToListAsync();
+        }
+
+        public async Task<Hotel> GetHotel(int id)
+        {
+            var hotel = await _context.Hotels.FindAsync(id);
+            if (hotel is null)
+            {
+                throw new Exception("There is no hotel.");
+            }
+            return hotel;
+        }
+
+        public async Task<Hotel> SetHotelStatus(int hotelId, HotelStatus status)
+        {
+            var hotel = await _context.Hotels.FindAsync(hotelId);
+            if (hotel is null)
+            {
+                throw new Exception("There is no hotel.");
+            }
+            hotel.Status = status;
+            await _context.SaveChangesAsync();
+            return hotel;
+        }
+
+        public async Task<Hotel> UpdateHotel(int hotelId, Hotel hotel)
+        {
+            var foundHotel = await _context.Hotels.FindAsync(hotelId);
+            if (foundHotel is null)
+            {
+                throw new Exception("There is no hotel.");
+            }
+            foundHotel.Status = hotel.Status;
+            foundHotel.Floor = hotel.Floor;
+            foundHotel.Address = hotel.Address;
+            foundHotel.Rooms = hotel.Rooms;
+            foundHotel.Classification = hotel.Classification;
+            foundHotel.Name = hotel.Name;
+
+            await _context.SaveChangesAsync();
+            return foundHotel;
+
+        }
+    }
+}
---
+++ el-proyecte-grande-backend/Repositories/HotelNs/IHotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using el_proyecte_grande_backend.Models.Entities;
+using el_proyecte_grande_backend.Models.Enums;
+
+namespace el_proyecte_grande_backend.Repositories.HotelNs
+{
+    public interface IHotelRepository
+    {
+        public Task<IEnumerable<Hotel>> GetAllHotels();
+        public Task<Hotel> GetHotel(int id);
+        public Task<Hotel> AddHotel(Hotel hotel);
+        public Task<Hotel> UpdateHotel(int hotelId, Hotel hotel);
+        public Task<Hotel> SetHotelStatus(int hotelId, HotelStatus status);
+    }
+}
