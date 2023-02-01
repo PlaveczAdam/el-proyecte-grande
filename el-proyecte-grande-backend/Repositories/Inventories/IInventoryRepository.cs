38910734bae0505bb13b274cc9027ee6ed9c3881
Pityken <pityken@gmail.com>
2023-02-01 16:18:42 +0100

    create InventoryRpository interface

---
+++ el-proyecte-grande-backend/Repositories/Inventories/IInventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using el_proyecte_grande_backend.Models.Entities;
+using el_proyecte_grande_backend.Models.Enums;
+
+namespace el_proyecte_grande_backend.Repositories.Inventories;
+
+public interface IInventoryRepository
+{
+	public Task<IEnumerable<Inventory>> GetAllInventories();
+	public Task<Inventory> GetInventoryById(int id);
+	public Task<Inventory> GetInventoryByHotel(int hotelId);
+	public Task<Inventory> AddInventory(Inventory inventory);
+	public Task<Inventory> UpdateInventory(Inventory inventory);
+}
