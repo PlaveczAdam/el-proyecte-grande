4f087d061a3afa961df265d4a8e089ca25e68149
Plavecz Adam <plaveczadam79@gmail.com>
2023-02-01 23:20:15 +0100

    add filter add database data

--- el-proyecte-grande-frontend/src/Components/Content/HotelModule/Hotels.jsx
+++ el-proyecte-grande-frontend/src/Components/Content/HotelModule/Hotels.jsx
@@ -1,91 +1,143 @@
-import * as React from 'react';
+import React, { useEffect, useState, useContext } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import EditIcon from '@mui/icons-material/Edit';
-import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
-import ContentPagination from '../../Shared/Pagination';
-import TextField from '@mui/material/TextField';
-
-function createData(id, name, address, status, stars, floors, rooms) {
-    return { id, name, address, status, stars, floors, rooms };
-}
-
-const rows = [
-    createData(1, 'Hotel Coffee', 'Lorem ipsum address', 'In use', 'Comfort', 4, 40),
-    createData(2, 'Hotel Menthol', 'Lorem ipsum address', 'In use', 'First class', 10, 100),
-    createData(3, 'Hotel Lorem', 'Lorem ipsum address', 'Renovation in progress', 'Comfort', 5, 50),
-    createData(4, 'Hotel Ipsum', 'Lorem ipsum address', 'Building in progress', 'Luxury', 10, 110),
-    createData(5, 'Hotel Grabde Superior', 'Lorem ipsum address', 'In use', 'Comfort', 12, 200),
-];
-
-
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import DisabledByDefaultIcon from "@mui/icons-material/DisabledByDefault";
+import ContentPagination from "../../Shared/Pagination";
+import TextField from "@mui/material/TextField";
+import HotelDataCell from "./HotelDataCell";
+import AddHotelModal from "./AddHotelModal";
+import EditHotelModal from "./EditHotelModal";
+import { HotelContext } from "./HotelContextProvider";
 
 const Hotels = () => {
-    return (
-        <div className="Hotels">
-            <Box sx={{ textAlign: 'center' }}>
-                <h2 >Hotel</h2>
-            </Box>
-            <Box sx={{ marginY: 1 }}>
-                <Grid container
-                    direction="row"
-                    alignItems="center"
-                    spacing={2}
-                >
-                    <Grid item xs={12} md={9} >
-                        <Button variant="text">Add new</Button>
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <TextField id="outlined-basic" label="Filter" variant="outlined" size="small" />
-                    </Grid>
-                </Grid>
-            </Box>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Id</TableCell>
-                            <TableCell align="center">Name</TableCell>
-                            <TableCell align="center">Address</TableCell>
-                            <TableCell align="center">Status</TableCell>
-                            <TableCell align="center">Stars</TableCell>
-                            <TableCell align="center">Floors</TableCell>
-                            <TableCell align="center">Rooms</TableCell>
-                            <TableCell align="center">Edit</TableCell>
-                            <TableCell align="center">Set status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row) => (
-                            <TableRow
-                                key={row.name}
-                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                            >
-                                <TableCell align="center">{row.id}</TableCell>
-                                <TableCell align="center">{row.name}</TableCell>
-                                <TableCell align="center">{row.address}</TableCell>
-                                <TableCell align="center">{row.status}</TableCell>
-                                <TableCell align="center">{row.stars}</TableCell>
-                                <TableCell align="center">{row.floors}</TableCell>
-                                <TableCell align="center">{row.rooms}</TableCell>
-                                <TableCell align="center"><Button variant="text"><EditIcon /></Button></TableCell>
-                                <TableCell align="center"><Button variant="text"><DisabledByDefaultIcon /></Button></TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <ContentPagination />
-        </div>
-    )
-}
+  const [rows, setRows] = useState([]);
+  const [filter, setFilter] = useState("");
+  const handleNewHotel = (hotel) => {
+    setRows([...rows, hotel]);
+  };
 
-export default Hotels
+  async function handleStatusChange(row) {
+    const status =
+      row.hotelStatus === context.enums.hotelStatus.values.InUse
+        ? context.enums.hotelStatus.values.RenovationInProgress
+        : context.enums.hotelStatus.values.InUse;
+    await fetch(`/api/hotel/${row.id}/${status}`, {
+        method:"PUT"
+    });
+    handleHotelUpdate({...row, hotelStatus:status});
+  }
+  const context = useContext(HotelContext);
+  const handleHotelUpdate = (hotel) => {
+    const newRows = [...rows];
+    const indexOfHotel = newRows.findIndex((x) => x.id === hotel.id);
+    newRows[indexOfHotel] = hotel;
+    setRows(newRows);
+  };
+  useEffect(() => {
+    async function getHotels() {
+      const response = await fetch("/api/hotel");
+      const hotels = await response.json();
+      setRows(hotels);
+    }
+    getHotels();
+  }, []);
+
+  function getAddressString(address) {
+    return `${address.postalCode} ${address.country}  ${address.region}  ${address.city}  ${address.addressLineOne}  ${address.addressLineTwo}`;
+  }
+
+  return (
+    <div className="Hotels">
+      <Box sx={{ textAlign: "center" }}>
+        <h2>Hotel</h2>
+      </Box>
+      <Box sx={{ marginY: 1 }}>
+        <Grid container direction="row" alignItems="center" spacing={2}>
+          <Grid item xs={12} md={9}>
+            <AddHotelModal onNewHotel={handleNewHotel} />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              id="outlined-basic"
+              label="Filter"
+              variant="outlined"
+              size="small"
+              onChange={(e)=>setFilter(e.target.value)}
+              value={filter}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Id</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Address</TableCell>
+              <TableCell align="center">Status</TableCell>
+              <TableCell align="center">Stars</TableCell>
+              <TableCell align="center">Floors</TableCell>
+              <TableCell align="center">Rooms</TableCell>
+              <TableCell align="center">Edit</TableCell>
+              <TableCell align="center">Set status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.filter(x => x.name.toLowerCase().includes(filter.toLowerCase())).map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <HotelDataCell data={row.id}></HotelDataCell>
+                <HotelDataCell data={row.name}></HotelDataCell>
+                <HotelDataCell
+                  data={getAddressString(row.address)}
+                ></HotelDataCell>
+                <HotelDataCell data={row.hotelStatus}></HotelDataCell>
+                <HotelDataCell data={row.classification}></HotelDataCell>
+                <HotelDataCell data={row.floor}></HotelDataCell>
+                <HotelDataCell data={row.rooms}></HotelDataCell>
+
+                <TableCell align="center">
+                  <EditHotelModal
+                    hotel={row}
+                    onHotelUpdate={handleHotelUpdate}
+                  />
+                </TableCell>
+                <TableCell align="center">
+                  <Button
+                    variant="text"
+                    onClick={() => handleStatusChange(row)}
+                  >
+                    <DisabledByDefaultIcon
+                      sx={{
+                        color:
+                          row.hotelStatus ===
+                          context.enums.hotelStatus.values.InUse
+                            ? "red"
+                            : "green",
+                      }}
+                    />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <ContentPagination />
+    </div>
+  );
+};
+
+export default Hotels;
