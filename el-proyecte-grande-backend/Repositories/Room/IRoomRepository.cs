8601891f4516bca3a18eda13f2de5b2a2ce44e1b
tercsd <tercsd@gmail.com>
2023-02-01 20:26:48 +0100

    implement RoomType and Accessory related repository methods and dtos in RoomController

--- el-proyecte-grande-backend/Repositories/Room/IRoomRepository.cs
+++ el-proyecte-grande-backend/Repositories/Room/IRoomRepository.cs
@@ -1,19 +1,30 @@
 ﻿using el_proyecte_grande_backend.Models.Dtos;
+using el_proyecte_grande_backend.Models.Entities;
 
 namespace el_proyecte_grande_backend.Repositories.Room;
 
 public interface IRoomRepository
 {
-    Task<IEnumerable<Models.Entities.Room>> GetAllRooms();
-    Task<IEnumerable<Models.Entities.Room>> GetAllRooms(long hotelId);
-    Task<IEnumerable<Models.Entities.Room>?> GetFilteredRooms(
+    Task<IEnumerable<RoomDto>> GetAllRooms();
+    Task<IEnumerable<RoomDto>> GetAllRooms(long hotelId);
+    Task<IEnumerable<RoomDto>?> GetFilteredRooms(
         string? hotelId,
         string? status, string? roomTypeId,
         string? maxPrice,
         string? date,
         string? accessible);
-    Task<Models.Entities.Room?> GetRoomById(long roomId);
-    Task<Models.Entities.Room?> AddRoom(NewRoom room);
-    Task<Models.Entities.Room?> UpdateRoom(long roomId, NewRoom room);
-    Task<Models.Entities.Room?> SetRoomStatus(long roomId, int status);
+    Task<RoomDto?> GetRoomById(long roomId);
+    Task<RoomDto?> AddRoom(NewRoom room);
+    Task<RoomDto?> UpdateRoom(long roomId, NewRoom room);
+    Task<RoomDto?> SetRoomStatus(long roomId, int status);
+
+    Task<IEnumerable<RoomTypeDto>> GetAllRoomTypes();
+    Task<RoomTypeDto?> GetRoomTypeById(long roomTypeId);
+    Task<RoomTypeDto?> AddRoomType(NewRoomType roomType);
+    Task<RoomTypeDto?> UpdateRoomType(long roomTypeId, NewRoomType roomType);
+
+    Task<IEnumerable<AccessoryDto>> GetAllAccessories();
+    Task<AccessoryDto?> GetAccessoryById(long accessoryId);
+    Task<AccessoryDto?> AddAccessory(NewAccessory accessory);
+    Task<AccessoryDto?> UpdateAccessory(long accessoryId, NewAccessory accessory);
 }
--- el-proyecte-grande-backend/Repositories/Room/RoomRepository.cs
+++ el-proyecte-grande-backend/Repositories/Room/RoomRepository.cs
@@ -4,6 +4,7 @@ using el_proyecte_grande_backend.Models.Entities;
 using el_proyecte_grande_backend.Models.Enums;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
+using System.Xml.Linq;
 
 namespace el_proyecte_grande_backend.Repositories.Room;
 
@@ -15,28 +16,42 @@ public class RoomRepository : IRoomRepository
         _context = context;
     }
 
-    public async Task<IEnumerable<Models.Entities.Room>> GetAllRooms()
+    public async Task<IEnumerable<RoomDto>> GetAllRooms()
     {
-        return await _context.Rooms
+        var rooms = await _context.Rooms
             .Include(r => r.Hotel)
             .Include(r => r.RoomType)
             .Include(r => r.Reservations)
             .AsNoTracking()
             .ToListAsync();
+
+        var roomDtos = new List<RoomDto>();
+        foreach (var room in rooms)
+        {
+            roomDtos.Add(CreateRoomDto(room));
+        }
+        return roomDtos;
     }
 
-    public async Task<IEnumerable<Models.Entities.Room>> GetAllRooms(long hotelId)
+    public async Task<IEnumerable<RoomDto>> GetAllRooms(long hotelId)
     {
-        return await _context.Rooms
+        var rooms = await _context.Rooms
             .Include(r => r.Hotel)
             .Include(r => r.RoomType)
             .Include(r => r.Reservations)
             .Where(r => r.Hotel.Id == hotelId)
             .AsNoTracking()
             .ToListAsync();
+
+        var roomDtos = new List<RoomDto>();
+        foreach (var room in rooms)
+        {
+            roomDtos.Add(CreateRoomDto(room));
+        }
+        return roomDtos;
     }
 
-    public async Task<IEnumerable<Models.Entities.Room>?> GetFilteredRooms(
+    public async Task<IEnumerable<RoomDto>?> GetFilteredRooms(
         string? hotelId,
         string? status,
         string? roomTypeId,
@@ -83,7 +98,7 @@ public class RoomRepository : IRoomRepository
         {
             var dateParsed = DateTime.TryParse(date, out DateTime filterDate);
             if (!dateParsed) return null;
-            roomsToFilter = roomsToFilter.Where(r => r.Reservations.Any(res => 
+            roomsToFilter = roomsToFilter.Where(r => r.Reservations.Any(res =>
                 (res.StartDate < filterDate) && (res.EndDate > filterDate))).ToList();
         }
         if (accessible != null)
@@ -93,21 +108,33 @@ public class RoomRepository : IRoomRepository
             roomsToFilter = roomsToFilter.Where(r => r.Accessible == filterAccessible).ToList();
         }
 
-        return roomsToFilter;
+        var roomDtos = new List<RoomDto>();
+        foreach (var room in roomsToFilter)
+        {
+            roomDtos.Add(CreateRoomDto(room));
+        }
+        return roomDtos;
     }
 
-    public async Task<Models.Entities.Room?> GetRoomById(long roomId)
+    public async Task<RoomDto?> GetRoomById(long roomId)
     {
-        return await _context.Rooms
+        var room = await _context.Rooms
             .Include(r => r.Hotel)
             .Include(r => r.RoomType)
             .Include(r => r.Reservations)
             .Where(r => r.Id == roomId)
             .AsNoTracking()
             .SingleOrDefaultAsync();
+
+        return CreateRoomDto(room);
     }
-    public async Task<Models.Entities.Room?> AddRoom(NewRoom room)
+    public async Task<RoomDto?> AddRoom(NewRoom room)
     {
+        if ((int)room.Status >= Enum.GetNames(typeof(RoomStatus)).Length)
+        {
+            return null;
+        }
+
         var relatedHotel = await _context.Hotels.Where(h => h.Id == room.HotelId).SingleOrDefaultAsync();
         var relatedRoomType = await _context.RoomTypes.Where(t => t.Id == room.RoomTypeId).SingleOrDefaultAsync();
         if (relatedHotel == null || relatedRoomType == null) return null;
@@ -123,24 +150,28 @@ public class RoomRepository : IRoomRepository
             Reservations = new List<Reservation>()
         };
 
-        if (roomToAdd.Hotel == null
-            || roomToAdd.RoomType == null
-            || (int)roomToAdd.Status >= Enum.GetNames(typeof(RoomStatus)).Length)
+        await _context.Rooms.AddAsync(roomToAdd);
+        await _context.SaveChangesAsync();
+        return CreateRoomDto(roomToAdd);
+    }
+    public async Task<RoomDto?> UpdateRoom(long roomId, NewRoom room)
+    {
+        if ((int)room.Status >= Enum.GetNames(typeof(RoomStatus)).Length)
         {
             return null;
         }
 
-        await _context.Rooms.AddAsync(roomToAdd);
-        await _context.SaveChangesAsync();
-        return roomToAdd;
-    }
-    public async Task<Models.Entities.Room?> UpdateRoom(long roomId, NewRoom room)
-    {
         var relatedHotel = await _context.Hotels.Where(h => h.Id == room.HotelId).SingleOrDefaultAsync();
         var relatedRoomType = await _context.RoomTypes.Where(t => t.Id == room.RoomTypeId).SingleOrDefaultAsync();
         if (relatedHotel == null || relatedRoomType == null) return null;
 
-        var roomToUpdate = await _context.Rooms.Where(r => r.Id == roomId).SingleOrDefaultAsync();
+        var roomToUpdate = await _context.Rooms
+            .Include(r => r.Hotel)
+            .Include(r => r.RoomType)
+            .Include(r => r.Reservations)
+            .Where(r => r.Id == roomId)
+            .SingleOrDefaultAsync();
+
         if (roomToUpdate == null) return null;
         roomToUpdate.Floor = room.Floor;
         roomToUpdate.DoorNo = room.DoorNo;
@@ -149,20 +180,19 @@ public class RoomRepository : IRoomRepository
         roomToUpdate.Hotel = relatedHotel;
         roomToUpdate.RoomType = relatedRoomType;
 
-        if (roomToUpdate.Hotel == null
-            || roomToUpdate.RoomType == null
-            || (int)roomToUpdate.Status >= Enum.GetNames(typeof(RoomStatus)).Length)
-        {
-            return null;
-        }
         await _context.SaveChangesAsync();
-
-        return roomToUpdate;
+        return CreateRoomDto(roomToUpdate);
     }
 
-    public async Task<Models.Entities.Room?> SetRoomStatus(long roomId, int status)
+    public async Task<RoomDto?> SetRoomStatus(long roomId, int status)
     {
-        var roomToUpdate = await _context.Rooms.Where(r => r.Id == roomId).SingleOrDefaultAsync();
+        var roomToUpdate = await _context.Rooms
+            .Include(r => r.Hotel)
+            .Include(r => r.RoomType)
+            .Include(r => r.Reservations)
+            .Where(r => r.Id == roomId)
+            .SingleOrDefaultAsync();
+
         if (roomToUpdate == null || status >= Enum.GetNames(typeof(RoomStatus)).Length)
         {
             return null;
@@ -170,7 +200,182 @@ public class RoomRepository : IRoomRepository
 
         roomToUpdate.Status = (RoomStatus)status;
         await _context.SaveChangesAsync();
-        return roomToUpdate;
+        return CreateRoomDto(roomToUpdate);
+    }
+
+
+
+    public async Task<IEnumerable<RoomTypeDto>> GetAllRoomTypes()
+    {
+        var roomTypes = await _context.RoomTypes
+            .Include(rt => rt.Accessories)
+            .AsNoTracking()
+            .ToListAsync();
+
+        var roomTypeDtos = new List<RoomTypeDto>();
+        foreach (var roomType in roomTypes)
+        {
+            roomTypeDtos.Add(CreateRoomTypeDto(roomType));
+        }
+        return roomTypeDtos;
+    }
+
+    public async Task<RoomTypeDto?> GetRoomTypeById(long roomTypeId)
+    {
+        var roomType = await _context.RoomTypes
+            .Include(rt => rt.Accessories)
+            .Where(rt => rt.Id == roomTypeId)
+            .AsNoTracking()
+            .SingleOrDefaultAsync();
+
+        return CreateRoomTypeDto(roomType);
+    }
+
+    public async Task<RoomTypeDto?> AddRoomType(NewRoomType roomType)
+    {
+        if ((int)roomType.RoomQuality >= Enum.GetNames(typeof(RoomQuality)).Length)
+        {
+            return null;
+        }
+
+        var roomTypeToAdd = new RoomType
+        {
+            Name = roomType.Name,
+            Price = roomType.Price,
+            RoomQuality = roomType.RoomQuality,
+            Accessories = new List<Accessory>()
+        };
+
+        await _context.RoomTypes.AddAsync(roomTypeToAdd);
+        await _context.SaveChangesAsync();
+        return CreateRoomTypeDto(roomTypeToAdd);
+    }
+
+    public async Task<RoomTypeDto?> UpdateRoomType(long roomTypeId, NewRoomType roomType)
+    {
+        if ((int)roomType.RoomQuality >= Enum.GetNames(typeof(RoomQuality)).Length)
+        {
+            return null;
+        }
+
+        var roomTypeToUpdate = await _context.RoomTypes
+            .Include(rt => rt.Accessories)
+            .Where(rt => rt.Id == roomTypeId)
+            .SingleOrDefaultAsync();
+        if (roomTypeToUpdate == null) return null;
+
+        roomTypeToUpdate.Name = roomType.Name;
+        roomTypeToUpdate.Price = roomType.Price;
+        roomTypeToUpdate.RoomQuality = roomType.RoomQuality;
+
+        await _context.SaveChangesAsync();
+        return CreateRoomTypeDto(roomTypeToUpdate);
+    }
+
+
+
+    public async Task<IEnumerable<AccessoryDto>> GetAllAccessories()
+    {
+        var accesories = await _context.Accessories
+            .Include(a => a.RoomType)
+            .AsNoTracking()
+            .ToListAsync();
+
+        var accessoryDtos = new List<AccessoryDto>();
+        foreach (var accessory in accesories)
+        {
+            accessoryDtos.Add(CreateAccessoryDto(accessory));
+        }
+        return accessoryDtos;
+    }
+
+    public async Task<AccessoryDto?> GetAccessoryById(long accessoryId)
+    {
+        var accessory = await _context.Accessories
+            .Include(a => a.RoomType)
+            .Where(a => a.Id == accessoryId)
+            .AsNoTracking()
+            .SingleOrDefaultAsync();
+
+        return CreateAccessoryDto(accessory);
+    }
+
+
+    public async Task<AccessoryDto?> AddAccessory(NewAccessory accessory)
+    {
+        var relatedRoomType = await _context.RoomTypes.Where(rt => rt.Id == accessory.RoomTypeId).SingleOrDefaultAsync();
+        if (relatedRoomType == null) return null;
+
+        var accessoryToAdd = new Accessory
+        {
+
+            Name = accessory.Name,
+            Quantity = accessory.Quantity,
+            RoomType = relatedRoomType,
+        };
+
+        await _context.Accessories.AddAsync(accessoryToAdd);
+        await _context.SaveChangesAsync();
+        return CreateAccessoryDto(accessoryToAdd);
+    }
+
+    public async Task<AccessoryDto?> UpdateAccessory(long accessoryId, NewAccessory accessory)
+    {
+        var accessoryToUpdate = await _context.Accessories.Where(a => a.Id == accessoryId).SingleOrDefaultAsync();
+        var relatedRoomType = await _context.RoomTypes.Where(rt => rt.Id == accessory.RoomTypeId).SingleOrDefaultAsync();
+        if (accessoryToUpdate == null || relatedRoomType == null) return null;
+
+        accessoryToUpdate.Name = accessory.Name;
+        accessoryToUpdate.Quantity = accessory.Quantity;
+        accessoryToUpdate.RoomType = relatedRoomType;
+
+        await _context.SaveChangesAsync();
+        return CreateAccessoryDto(accessoryToUpdate);
+    }
+
+    private RoomDto? CreateRoomDto(Models.Entities.Room? room)
+    {
+        if (room == null) return null;
+        var dto = new RoomDto()
+        {
+            Id = room.Id,
+            Floor = room.Floor,
+            DoorNo = room.DoorNo,
+            Accessible = room.Accessible,
+            Status = room.Status,
+            HotelId = room.Hotel.Id,
+            RoomTypeId = room.RoomType.Id,
+            ReservationIds = new(),
+        };
+        room.Reservations.ToList().ForEach(r => { dto.ReservationIds.Add(r.Id); });
+        return dto;
+    }
+
+    private RoomTypeDto? CreateRoomTypeDto(RoomType? roomType)
+    {
+        if (roomType == null) return null;
+        var dto = new RoomTypeDto()
+        {
+            Id = roomType.Id,
+            Name = roomType.Name,
+            Price = roomType.Price,
+            AccessoryIds = new(),
+        };
+        roomType.Accessories.ToList().ForEach(rt => { dto.AccessoryIds.Add(rt.Id); });
+        return dto;
+    }
+
+    private AccessoryDto? CreateAccessoryDto(Accessory? accessory)
+    {
+        if (accessory == null) return null;
+        var dto = new AccessoryDto()
+        {
+            Id = accessory.Id,
+            Name = accessory.Name,
+            Quantity = accessory.Quantity,
+            RoomTypeId = accessory.RoomType.Id,
+        };
+        return dto;
     }
 
 }
