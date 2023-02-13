6d6f04f8f86da096b05b2399fcb18aeb66acc7e7
tercsd <tercsd@gmail.com>
2023-02-13 15:48:19 +0100

    add Role entity

---
+++ el-proyecte-grande-backend/Models/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace el_proyecte_grande_backend.Models.Entities
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
