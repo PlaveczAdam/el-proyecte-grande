b26fadd6c82bb2b5b1e651ee05f6451d58f81a89
tercsd <tercsd@gmail.com>
2023-02-14 15:47:58 +0100

    add user presence in Header component

--- el-proyecte-grande-frontend/src/Components/Shared/Header.jsx
+++ el-proyecte-grande-frontend/src/Components/Shared/Header.jsx
@@ -1,30 +1,42 @@
+import { useContext } from "react";
+import { AuthContext } from "../Shared/AuthContext";
+
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
+import Tooltip from "@mui/material/Tooltip";
 
 const Header = () => {
-    return (
-        <div className="Header">
-            <Paper>
-                <Grid container alignItems="right" spacing={2}>
-                    <Grid item xs={12} md={9}>
+  const auth = useContext(AuthContext);
+  return (
+    <div className="Header">
+      <Paper>
+        <Grid container alignItems="right" spacing={2}>
+          <Grid item xs={12} md={8}></Grid>
+          <Grid item xs={12} md={4}>
+            <div className="User">
+              <span>Logged in: </span>
+              <Tooltip
+                title={
+                  "Roles: " + (auth.user.roles ? auth.user.roles : "No roles")
+                }
+              >
+                <span>
+                  <b>{auth.user.username ? auth.user.username : "N/A"}</b> (
+                  {auth.user.email ? auth.user.email : "N/A"})
+                </span>
+              </Tooltip>
+              <Button onClick={auth.logout}>Logout</Button>
+            </div>
+          </Grid>
+        </Grid>
+        <Box sx={{ marginY: 2, textAlign: "center" }}>
+          <h1>Grande management</h1>
+        </Box>
+      </Paper>
+    </div>
+  );
+};
 
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <div className="User">
-                        <span>Logged in: Admin</span>
-                        <Button>Logout</Button>
-                    </div>
-                </Grid>
-            </Grid>
-            <Box sx={{ marginY: 2, textAlign: 'center' }}>
-                <h1>Grande management</h1>
-            </Box>
-        </Paper >
-        </div >
-
-    )
-}
-
-export default Header
+export default Header;
