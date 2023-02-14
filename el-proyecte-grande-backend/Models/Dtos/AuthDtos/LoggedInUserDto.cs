ba41e221685d14da069cff69e5a35ab6fd9aa109
Quvasz <quvasz87@gmail.com>
2023-02-14 14:56:24 +0100

    add AuthDtos

---
+++ el-proyecte-grande-backend/Models/Dtos/AuthDtos/LoggedInUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace el_proyecte_grande_backend.Models.Dtos.AuthDtos
+{
+    public class LoggedInUserDto
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set;}
+    }
+}
