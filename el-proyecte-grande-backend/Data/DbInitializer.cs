30bb2b10e9dbfec54a809677124026de9dde4a8c
Quvasz <quvasz87@gmail.com>
2023-02-14 14:59:27 +0100

    add PasswordHasher to DbInitializer class

--- el-proyecte-grande-backend/Data/DbInitializer.cs
+++ el-proyecte-grande-backend/Data/DbInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using el_proyecte_grande_backend.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace el_proyecte_grande_backend.Data
 {
@@ -14,30 +15,33 @@ namespace el_proyecte_grande_backend.Data
 
         public void Seed()
         {
-                if (_context.Roles.Count() == 0)
-                {
-                    _context.Roles.AddRange(new Role[] {
+            if (_context.Roles.Count() == 0)
+            {
+                _context.Roles.AddRange(new Role[] {
                     new Role {Name = "Admin" },
                     new Role {Name = "Manager" },
                     new Role {Name = "Receptionist" },
                     new Role {Name = "Staff" }
                     });
 
-                    _context.SaveChanges();
+                _context.SaveChanges();
 
-                    _context.Users.Add(
-                            new User()
-                            {
-                                Name = "Root",
-                                Email = "",
-                                Password = "",
-                                IsActive = false,
-                                Roles = new Role[] { _context.Roles.First(r => r.Name == "Admin") }
-                            }
-                    );
+                User user = new User()
+                {
+                    Name = "Root",
+                    Email = "",
+                    Password = "",
+                    IsActive = false,
+                    Roles = new Role[] { _context.Roles.First(r => r.Name == "Admin") }
+                };
 
-                    _context.SaveChanges();
-                }; 
+                PasswordHasher<User> hasher = new PasswordHasher<User>();
+                user.Password = hasher.HashPassword(user, "1234");
+
+                _context.Users.Add(user);
+
+                _context.SaveChanges();
+            }; 
         }
     }
 }
