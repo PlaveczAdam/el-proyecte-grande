d9f437bded2d95294aa07a0bfbdf6d06f4ac8ab9
Plavecz Adam <plaveczadam79@gmail.com>
2023-02-14 16:52:45 +0100

    implement GetAllUser endpoint and dto conversion

--- el-proyecte-grande-backend/Controllers/UserController.cs
+++ el-proyecte-grande-backend/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using el_proyecte_grande_backend.Services.UserServices;
+﻿using el_proyecte_grande_backend.Models.Dtos.UserNs;
+using el_proyecte_grande_backend.Models.Entities;
+using el_proyecte_grande_backend.Services.UserServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace el_proyecte_grande_backend.Controllers
@@ -11,6 +13,35 @@ namespace el_proyecte_grande_backend.Controllers
         public UserController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserDto>> GetAllUser()
+        {
+            return (await _userService.GetAllUsers()).Select(MapUserToDto);
+        }
+
+        private static UserDto MapUserToDto(User user)
+        {
+            return new UserDto(
+                Name: user.Name,
+                Email: user.Email,
+                Password: user.Password,
+                IsActive: user.IsActive,
+                Roles: user.Roles
+            );
+        }
+
+        private static User MapDtoToHotel(UserDto userDto)
+        {
+            return new User()
+            {
+                Name = userDto.Name,
+                Email = userDto.Email,
+                Password = userDto.Password,
+                IsActive = userDto.IsActive,
+                Roles = userDto.Roles
+            };
         }
     }
 }
